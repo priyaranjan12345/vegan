1fa4850eb36a6bacc9956f4f5317aa164e9a67a2
Priyaranjan <priyaranjanmantri@gmail.com>
2025-03-05 21:26:25 +0530

    feat: splash guard

--- lib/src/core/router/app_router_guard.dart
+++ lib/src/core/router/app_router_guard.dart
@@ -1,10 +1,11 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:vegan/src/core/router/app_router.gr.dart';
 
-class AppRouterGuard extends AutoRouteGuard {
+class SpalshRouteGuard extends AutoRouteGuard {
   @override
-  void onNavigation(NavigationResolver resolver, StackRouter router) {
-    // if user is authenticated we continue
-    // resolver.next(true);
-    // router.pushPath('dashrouter');
+  void onNavigation(NavigationResolver resolver, StackRouter router) async {
+    resolver.next(true);
+    // await Future.delayed(const Duration(seconds: 4));
+    router.replaceAll([const DashboardRoute()]);
   }
 }
