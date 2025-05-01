e7b28994085f48425ea6177e8af4d2fe80dba9e8
Priyaranjan <priyaranjanmantri@gmail.com>
2025-05-01 15:42:52 +0530

    feat: added route

--- lib/src/core/router/app_router_guard.dart
+++ lib/src/core/router/app_router_guard.dart
@@ -1,15 +1,16 @@
 import 'package:auto_route/auto_route.dart';
 
+import 'app_router.gr.dart';
 import '../../app/app.dart';
 
 class SpalshRouteGuard extends AutoRouteGuard {
   @override
   void onNavigation(NavigationResolver resolver, StackRouter router) async {
-    await init();
+    // await init();
     resolver.next(true);
     // any loading operations
     // await Future.delayed(const Duration(seconds: 4));
-    // router.replaceAll([const DashboardRoute()]);
+    router.popAndPush(const DashboardRoute());
   }
 }
 
