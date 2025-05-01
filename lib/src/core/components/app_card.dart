f73833b94c4603fcb9f808b5bbd25adbd5d6c82d
Priyaranjan <priyaranjanmantri@gmail.com>
2025-05-01 21:28:31 +0530

    feat: app card

--- lib/src/core/components/app_card.dart
+++ lib/src/core/components/app_card.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
 
+import 'app_image.dart';
+
 class AppCard extends StatelessWidget {
-  const AppCard({super.key});
+  const AppCard({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.subTitle,
+    this.onTap,
+    this.height,
+    this.width,
+  });
+
+  final double? height;
+  final double? width;
+  final String imageUrl;
+  final String title;
+  final String subTitle;
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      child: SizedBox(
+        height: height,
+        width: width,
+        child: Column(
+          spacing: 8,
+          children: [
+            AppImage(
+              width: 60,
+              height: 60,
+              fit: BoxFit.cover,
+              imageUrl: imageUrl,
+            ),
+            Text(
+              title,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              subTitle,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
