114983bf571a52afcdd2ac8a11a3e26d158a9066
Priyaranjan <priyaranjanmantri@gmail.com>
2025-05-03 17:48:57 +0530

    feat: api config

---
+++ lib/src/core/base/yt_api_body.dart
@@ -0,0 +1,58 @@
+abstract class YtApiBody {
+  static const context = {
+    "context": {
+      "client": {
+        "clientName": "WEB_REMIX",
+        "clientVersion": "1.20220404.01.00",
+        "hl": "en",
+        "gl": "IN",
+        "browserName": "Chrome",
+        "browserVersion": "91.0.4472.124",
+        "osName": "Android",
+        "osVersion": "13.0",
+        "platform": "MOBILE"
+      }
+    }
+  };
+
+  static const home = {
+    "browseId": "FEmusic_home",
+    ...context,
+  };
+
+  static const moods = {
+    "browseId": "FEmusic_moods_and_genres",
+    ...context,
+  };
+
+  Map<String, Object> moodsContent(String params) => {
+        "browseId": "FEmusic_moods_and_genres_category",
+        "params": params,
+        ...context,
+      };
+
+  Map<String, Object> playlistContent(String browseId) => {
+        "browseId": browseId,
+        ...context,
+      };
+
+  static const global = {
+    "browseId": "FEmusic_home",
+    "context": {
+      "client": {
+        "clientName": "WEB_REMIX",
+        "clientVersion": "1.20220708.01.00",
+        "hl": "en",
+        "gl": "US",
+        "experimentIds": [],
+        "experimentsToken": "",
+        "browserName": "Chrome",
+        "browserVersion": "91.0.4472.124",
+        "osName": "Android",
+        "osVersion": "13.0",
+        "platform": "MOBILE"
+      },
+      "user": {"lockedSafetyMode": false}
+    }
+  };
+}
