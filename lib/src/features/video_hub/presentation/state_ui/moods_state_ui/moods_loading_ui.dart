e6edbf23c3346303d5a542c519069edabadf2590
Priyaranjan <priyaranjanmantri@gmail.com>
2025-05-03 22:54:31 +0530

    feat: moods loading ui

--- lib/src/features/video_hub/presentation/state_ui/moods_state_ui/moods_loading_ui.dart
+++ lib/src/features/video_hub/presentation/state_ui/moods_state_ui/moods_loading_ui.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vegan/src/core/components/components.dart';
 
-import '../../../../../core/theme/app_colors.dart';
-
 class MoodsLoadingUi extends StatelessWidget {
   const MoodsLoadingUi({super.key});
 
@@ -18,15 +16,11 @@ class MoodsLoadingUi extends StatelessWidget {
           vertical: 8,
         ),
         itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
+          return const Padding(
+            padding: EdgeInsets.all(8.0),
             child: AppShimmer(
               child: Chip(
-                label: Container(
-                  height: 10,
-                  width: 20,
-                  color: AppColors.midGray,
-                ),
+                label: Text('Moods Label'),
               ),
             ),
           );
