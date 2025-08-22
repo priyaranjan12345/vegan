55ae46c203f469c260c137060b1ae9f2d7f07927
Priyaranjan <priyaranjanmantri@gmail.com>
2025-08-22 21:20:40 +0530

    fix: test file error

--- packages/feature_config/test/feature_config_test.dart
+++ packages/feature_config/test/feature_config_test.dart
@@ -1,12 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:feature_config/feature_config.dart';
-
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+  // todo: test cases
 }
