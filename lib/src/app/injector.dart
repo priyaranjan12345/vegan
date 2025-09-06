a41ba229c1a8d967f33428544403a05b861422ac
Priyaranjan <priyaranjanmantri@gmail.com>
2025-09-06 17:27:49 +0530

    feat: removed unused import

--- lib/src/app/injector.dart
+++ lib/src/app/injector.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
