da74282a956cd437b3b2e689c2d6792b71f3e416
Priyaranjan <priyaranjanmantri@gmail.com>
2025-09-06 17:28:31 +0530

    feat: settings Ui

--- lib/src/features/more/more_page.dart
+++ lib/src/features/more/more_page.dart
@@ -7,6 +7,52 @@ class MorePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          SwitchListTile(
+            title: Row(
+              children: const [
+                Icon(Icons.color_lens_rounded),
+                SizedBox(width: 16),
+                Text('Dark Theme'),
+              ],
+            ),
+            value: true,
+            onChanged: (v) {},
+          ),
+          ListTile(
+            title: const Text('Language'),
+            leading: const Icon(Icons.language),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Sign In'),
+            leading: const Icon(Icons.login),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Account Details'),
+            leading: const Icon(Icons.account_circle),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('About App'),
+            leading: const Icon(Icons.info),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Version'),
+            leading: const Icon(Icons.update),
+            subtitle: const Text('1.0.0'),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
